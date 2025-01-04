81380ad536a9c0fab299721e59c0a3ce033599bc
VasKuzn <113028860+VasKuzn@users.noreply.github.com>
2025-01-05 01:48:16 +0500

    Добавлена кнопка добавления контактов

--- reactplusdotnet.client/src/App.jsx
+++ reactplusdotnet.client/src/App.jsx
@@ -1,15 +1,26 @@
 //import { useEffect, useState } from 'react';
 import './App.css';
 import TableContact from "./layout/tableContact/TableContact"
-
-const contacts = [{ id: 1, name: "John", phoneNumber: 231321, email: "1@1.ru" },
-{ id: 2, name: "Clara", phoneNumber: 543543, email: "2@2.ru" },
-{ id: 3, name: "Vitya", phoneNumber: 786876, email: "3@3.ru" },
-];
-
-
+import {useState} from "react"
 const App = () =>
 {
+    const [contacts, SetContacts] = useState([{ id: 1, name: "John", phoneNumber: 231321, email: "1@1.ru" },
+    { id: 2, name: "Clara", phoneNumber: 543543, email: "2@2.ru" },
+    { id: 3, name: "Vitya", phoneNumber: 786876, email: "3@3.ru" },
+    ]
+    ); 
+    const addContact = () => {
+        const newID = contacts.sort((x, y) => x.id - y.id)[contacts.length - 1].id + 1;
+        const item =
+        {
+            id: newID,
+            name: "John",
+            phoneNumber: 231321,
+            email: "1@1.ru"
+        };
+        SetContacts([...contacts,item]);
+    }
+
     return(
         <div className="container mt-5">
             <div className = "card">
@@ -18,6 +29,11 @@ const App = () =>
                 </div>
                 <div className="card-body">
                     <TableContact contactlist={contacts} />
+                </div>
+                <div>
+                    <button className="btn btn-primary"
+                        onClick={() => {addContact()} }>Add
+                    </button>
                 </div>
             </div>
         </div>
